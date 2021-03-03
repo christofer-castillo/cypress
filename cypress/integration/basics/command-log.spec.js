ec70532312d266f6527a7d85d8d5d8170d44c31f
christofercastillo <chris.castillo@aviture.us.com>
2021-03-02 21:28:02 -0600

    Added a command logging test to display the differences.

---
+++ cypress/integration/basics/command-log.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('Command Logging', () => {
+
+    it('should display cy.log() information', () => {
+        cy.log('Origin', window.origin);
+
+        cy.visit('https://example.cypress.io/commands/actions');
+
+        cy.log('Cypress information', window.Cypress);
+    });
+
+    it('should display Cypress.log() information', () => {
+        cy.visit('https://example.cypress.io/commands/local-storage');
+        cy.contains('Populate Local Storage').click();
+        cy.seedLocalStorage('Las Vegas', 'Raiders').then(() => {
+            cy.log(localStorage.getItem('Las Vegas'));
+        });
+        cy.log(localStorage.getItem('Las Vegas'));
+        cy.log(Math.floor(Math.random() * 10));
+    });
+});
