816ec30bf444f0df8092e85055c392216f9d888f
christofercastillo <chris.castillo@aviture.us.com>
2021-04-30 23:08:39 -0500

    Breakdown of setting viewports on mdn website.

---
+++ cypress/integration/basics/responsive.spec.ts
@@ -0,0 +1,47 @@
+context('Cypress Viewport Options', () => {
+
+    beforeEach(() => cy.visit('https://developer.mozilla.org/en-US/'));
+
+    describe('Dynamically testing the viewport', () => {
+
+        const viewportSizes = [[1920, 1080], [1366, 1024], [844, 390], 'iphone-x']; // full hd, iPad Pro, iPhone 12 Pro
+
+        viewportSizes.forEach(i => {
+            it(`should visit the webpage with the viewport size: ${i}`, () => {
+
+                if (Cypress._.isArray(i)){
+                    cy.viewport(i[0], i[1]);
+                } else {
+                    // @ts-ignore
+                    cy.viewport(i);
+                };
+    
+                cy.get('[aria-label="MDN Web Docs"]').should('be.visible');
+            });
+        });
+    });
+
+    describe('Responsive testing via cy.viewport() presets', () => {
+
+        it('should set the viewport size', () => {
+            cy.viewport('iphone-x');
+
+            cy.get('.main-menu-toggle').click();
+
+            cy.get('#main-q').should('have.attr', 'placeholder', 'Search MDN');
+        });
+
+        it('should set the viewport size: ipad', () => {
+            cy.viewport('ipad-2');
+
+            cy.get('#main-q').type('responsive design{enter}');
+        });
+
+        it('should set the viewport size with a preset and orientation', () => {
+            cy.viewport('ipad-mini', 'landscape'); // default will be portrait
+
+            cy.get('#main-q').type('responsive design{enter}');
+        });
+    });
+
+});
